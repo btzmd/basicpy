de287c59bb4f6e952ea4388eb4776d41f85d7bda
btzmd <>
2022-03-14 09:10:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#string statement
+print("Hello world")
+print("my handles btzmd")
+
+#variables
+x = 10
+y = 11
+sum = x = y
+print("The sum is ", sum)
+
+#sample login 
+fname = input('Enter First Name:' )
+lname = input('Enter Last Name:')
+fullname = fname + lname
+print('Your name is '+ fullname)
